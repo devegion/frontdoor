351b56d9957e98b62a426aff9b0ad21e9e659b16
David-Pavel Marian <david.pavel2906@gmail.com>
2025-10-19 01:26:04 +0300

    FIX initial static build for cloudflare hosting

--- src/app/[locale]/services/consulting/page.tsx
+++ src/app/[locale]/services/consulting/page.tsx
@@ -1,5 +1,18 @@
-import { Incoming } from '@/shared/components/layout/Incoming';
+import { routing } from '@/i18n/routing';
+import { Incoming } from '@/shared/components/layout';
+import { hasLocale } from 'next-intl';
+import { setRequestLocale } from 'next-intl/server';
+import { notFound } from 'next/navigation';
 
-export default function ConsultingPage() {
+export default async function ConsultingPage({
+  params,
+}: PageProps<'/[locale]/services/web-development'>) {
+  const { locale } = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+  setRequestLocale(locale);
+
   return <Incoming />;
 }
